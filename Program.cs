331d92c9ce6aa74e342e268e804fe5fcf2ec899b
MatheusKhairallah <playermaster10like@gmail.com>
2020-02-28 22:23:41 -0300

    Adicionado programa c#

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can't stay at home, can't stay at school");
+            Console.WriteLine("Old folks say 'You poor little fool'");
+            Console.WriteLine("Down the streets I'm the girl next door");
+            Console.WriteLine("I'm the fox you've been waiting for");
+            Console.WriteLine("Hello, daddy. Hello, mom");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hello world! I'm your wild girl");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Stone age love and strange sounds too");
+            Console.WriteLine("Come on, baby, let me get to you");
+            Console.WriteLine("Bad nights causing teenage blues");
+            Console.WriteLine("Get down ladies, you've got nothin' to lose");
+            Console.WriteLine("Hello, daddy. Hello, mom");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hello world! I'm your wild girl");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hello, daddy. Hello, mom");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hello world! I'm your wild girl");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hey, street boy, want some style?");
+            Console.WriteLine("Your dead end dreams don't make you smile");
+            Console.WriteLine("I'll give you something to live for");
+            Console.WriteLine("Have you and grab you until you're sore");
+            Console.WriteLine("Hello, daddy. Hello, mom");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Hello world! I'm your wild girl");
+            Console.WriteLine("I'm your ch-ch-ch-cherry bomb!");
+            Console.WriteLine("Cherry bomb!");
+            Console.WriteLine("Cherry bomb!");
+            Console.WriteLine("Cherry bomb!");
+            Console.WriteLine("Cherry bomb!");
+        }
+    }
+}
